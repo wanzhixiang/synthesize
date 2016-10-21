c2da18b4e84d68f63ba7aa6a5ecc930dbd656122
wanzhixiang <timoheader@163.com>
2016-10-21 14:18:40 +0800

    项目名称以及构建

---
+++ action/src/main/java/com/wan/synehesize/web/action/LoginController.java
@@ -0,0 +1,22 @@
+package com.wan.synehesize.web.action;
+
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ * Created by wzx on 2016/7/28.
+ */
+@Controller
+@RequestMapping(value = "/wzx")
+public class LoginController {
+
+    private static Logger logger = Logger.getLogger(LoginController.class);
+
+    @RequestMapping(value = "/index")
+    public String index(){
+        return "WEB-INF/index";
+    }
+}
