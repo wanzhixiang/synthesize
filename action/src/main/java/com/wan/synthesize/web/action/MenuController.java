6f44268266d00e1cb7eee44eec2eea8d893c87a0
timoheader@163.com <w333sd1125>
2017-02-07 19:44:48 +0800

    ssm整合

--- action/src/main/java/com/wan/synthesize/web/action/MenuController.java
+++ action/src/main/java/com/wan/synthesize/web/action/MenuController.java
@@ -1,8 +1,14 @@
 package com.wan.synthesize.web.action;
 
+import com.wan.synthesize.baseenum.ConsisEnum;
+import com.wan.synthesize.common.ReturnResult;
+import com.wan.synthesize.domain.Menu;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * Created by wzx on 2016/12/27.
@@ -18,8 +24,13 @@ public class MenuController {
      */
     @RequestMapping(value = "/getMenu")
     @ResponseBody
-    public void getMenuTree() {
-
+    public ReturnResult getMenuTree(HttpServletRequest request) {
+        ReturnResult returnResult = new ReturnResult();
+        List<Menu> menus = ( List<Menu>) request.getSession().getAttribute(ConsisEnum.MENU_SESSION.name());
+        if (menus!=null && menus.size()>0){
+            returnResult.setData(menus);
+        }
+        return returnResult;
     }
 
     /**
